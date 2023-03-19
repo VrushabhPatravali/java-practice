4caa1e5861884df8cccabdcbbd931cbb62054d38
VrushabhPatravali <vrushabh.p.patravali@gmail.com>
2023-03-18 20:16:49 -0400

    new exercises

--- src/com/pragra/Exercise22.java
+++ src/com/pragra/Exercise22.java
@@ -2,6 +2,18 @@ package com.pragra;
 
 public class Exercise22 {
     public static void main(String[] args) {
+        System.out.println(shouldWakeUp(true, 7));
+    }
 
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (!barking) {
+            return false;
+        } else {
+            if (hourOfDay < 0 || hourOfDay > 23 || (hourOfDay >= 8 && hourOfDay <= 22)) {
+                return false;
+            } else {
+                return true;
+            }
+        }
     }
 }
---
+++ src/com/pragra/Exercise25.java
@@ -0,0 +1,4 @@
+package com.pragra;
+
+public class Exercise25 {
+}
