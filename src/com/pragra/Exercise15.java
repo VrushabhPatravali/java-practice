bbdeea5f3c8575494a623a789fb2fc8f73ed8712
VrushabhPatravali <vrushabh.p.patravali@gmail.com>
2023-03-29 10:36:22 -0400

    new feature method

--- src/com/pragra/Exercise15.java
+++ src/com/pragra/Exercise15.java
@@ -2,6 +2,11 @@ package com.pragra;
 
 public class Exercise15 {
     public static void main(String[] args) {
+
+        feature();
+    }
+
+    public static void feature() {
         System.out.println("new feature");
     }
 }
